0eecd9e36b1d49f675741a8ebe535c529c548d96
Sulaiman <sloom1415@gmail.com>
2018-10-26 22:44:12 +0300

    configure port for heroku

--- FormServer.py
+++ FormServer.py
@@ -1,6 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import parse_qs
-import Udacian
+from os import environ
 
 memory = []
 form = '''<!DOCTYPE html>
@@ -25,39 +25,40 @@ form = '''<!DOCTYPE html>
 
 
 class MessageHandler(BaseHTTPRequestHandler):
-  def do_POST(self):
-    length = int(self.headers.get('content-length', 0))
-    data = self.rfile.read(length).decode()
+    def do_POST(self):
+        length = int(self.headers.get('content-length', 0))
+        data = self.rfile.read(length).decode()
 
-    info = parse_qs(data)
-    memory.append('''
+        info = parse_qs(data)
+        memory.append('''
     name: {}
     city: {}
     enrollment: {}
     nanodegree: {}
     status: {}
     '''.format(
-      info['name'][0],
-      info['city'][0],
-      info['enrollment'][0],
-      info['nanodegree'][0],
-      info['status'][0]
-      )
-    )
+            info['name'][0],
+            info['city'][0],
+            info['enrollment'][0],
+            info['nanodegree'][0],
+            info['status'][0]
+        )
+        )
 
-    self.send_response(303)
-    self.send_header('Location', '/')
-    self.end_headers()
+        self.send_response(303)
+        self.send_header('Location', '/')
+        self.end_headers()
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-plain', 'text/html; charset=utf-8')
-    self.end_headers()
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-plain', 'text/html; charset=utf-8')
+        self.end_headers()
 
-    self.wfile.write(form.format('\n'.join(memory)).encode())
+        self.wfile.write(form.format('\n'.join(memory)).encode())
 
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = HTTPServer(server_address, MessageHandler)
     httpd.serve_forever()
